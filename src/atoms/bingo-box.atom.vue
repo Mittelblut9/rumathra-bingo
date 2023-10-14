b267e7f690e136c96845b4df355d671455cf49c3
Mittelblut9 <info@blackdayz.de>
2023-10-14 14:47:20 +0200

    feat: add click mechanic

---
+++ src/atoms/bingo-box.atom.vue
@@ -0,0 +1,47 @@
+<template>
+    <input hidden 
+        :data-fixed="fixed" 
+        :data-selected="selected"
+        :data-color="color"
+        :data-multi-select-number="multiSelectNumber"
+        :data-multi-select-options="multiSelectOptions"
+        data-times-pressed="0"
+    />
+    <p :class="[fixed ? 'bold big' : '']" class="center user-select-none pointer-events-none">{{ text }}</p>
+
+</template>
+
+<script>
+export default {
+    name: 'BingoBoxAtom',
+    props: {
+        fixed: {
+            type: Boolean,
+            default: false
+        },
+        selected: {
+            type: Boolean,
+            default: false
+        },
+        text: {
+            type: String,
+            default: ''
+        },
+        color: {
+            type: String,
+            default: ''
+        },
+        multiSelectNumber: {
+            type: Number,
+            default: 0
+        },
+        multiSelectOptions: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {};
+    },
+};
+</script>
--- src/components/bingo.component.vue
+++ src/components/bingo.component.vue
@@ -2,9 +2,15 @@
     <section class="section-bingo">
         <table>
             <tr v-for="tableData in tableData" :key="tableData">
-                <td v-for="tableContent in tableData" :key="tableContent">
-                    <input hidden :data-fixed="tableContent.fixed" :data-selected="tableContent.selected" />
-                    <p :class="[tableContent.fixed ? 'bold big' : '']" class="center user-select-none">{{ tableContent.text }}</p>
+                <td v-for="tableContent in tableData" :key="tableContent" @click="selectTarget">
+                    <BingoBoxAtom 
+                        :fixed="tableContent.fixed" 
+                        :selected="tableContent.selected" 
+                        :text="tableContent.text" 
+                        :color="tableContent.color"
+                        :multiSelectNumber="tableContent.multiSelectNumber"
+                        :multiSelectOptions="JSON.stringify(tableContent?.multiSelectOptions)"
+                    />
                 </td>
             </tr>
         </table>
@@ -13,12 +19,57 @@
 
 <script>
 import tableData from '@/assets/json/bingo.json';
+import BingoBoxAtom from '@/atoms/bingo-box.atom.vue';
 export default {
     name: 'BingoComponent',
     data() {
         return {
-            tableData: tableData.bingo
+            tableData: tableData.bingo,
         };
+    },
+    components: {
+        BingoBoxAtom
+    },
+    methods: {
+        selectTarget(e) {
+            const target = e.target;
+            const input = target.querySelector('input');
+
+            const fixed = input.dataset.fixed === 'true';
+            if(fixed) {
+                return;
+            }
+            
+            const timesPressed = parseInt(input.dataset.timesPressed);
+            let selected = input.dataset.selected === 'true';
+            let color = input.dataset.color;
+            let resetColor = false;
+            let resetTimesPressed = false;
+
+            const multiSelectNumber = parseInt(input.dataset.multiSelectNumber);
+            let multiSelectOptions = [];
+
+            if(multiSelectNumber > 0) {
+                multiSelectOptions = JSON.parse(input.dataset?.multiSelectOptions);
+            }
+            
+            if (multiSelectNumber > 0 && timesPressed > 0)  {
+                color = multiSelectOptions[`color${timesPressed}`];
+               
+                if(!color) {
+                    selected = false;
+                    resetColor = true;
+                    resetTimesPressed = true;
+                }
+            }else {
+                selected = !selected;
+            }
+
+            input.dataset.selected = selected;
+            input.dataset.timesPressed = resetTimesPressed ? 0 : timesPressed + 1;
+            target.style.backgroundColor = (selected && multiSelectNumber === 0) || resetColor ? '' : color;	
+
+        }
     }
 };
 </script>
