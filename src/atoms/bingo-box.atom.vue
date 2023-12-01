97502c984a9ea2b18f447c17376de6b8d0abd9af
Mittelblut9 <sauer.benedikt@outlook.de>
2023-12-01 14:43:02 +0100

    feat: make font dynamic size

--- src/atoms/bingo-box.atom.vue
+++ src/atoms/bingo-box.atom.vue
@@ -8,7 +8,7 @@
         data-times-pressed="0"
         data-times-rounds="0"
     />
-    <p :class="[fixed ? 'bold big' : '']" class="center user-select-none pointer-events-none">{{ text }}</p>
+    <p :class="[fixed ? 'bold big' : '']" class="center user-select-none pointer-events-none" editable="true">{{ text }}</p>
     <div class="bingo-box-multi pointer-events-none display-flex justify-content-space-between" v-if="multiSelectNumber > 0">
         <div v-for="i in multiSelectNumber" :key="i" class="bingo-box-multi--item pointer-events-none" :style="{ backgroundColor: multiSelectOptions[`color${i}`] }">
             <input hidden :data-multi-select-number="i"/>
@@ -48,5 +48,35 @@ export default {
     data() {
         return {};
     },
+    mounted() {
+        this.$nextTick(() => {
+            const el = this.$el;
+            this.changeFontSize(el);
+        });
+    },
+    methods: {
+        changeFontSize(el) {
+            const pTag = el.nextElementSibling.nextElementSibling;
+            const text = pTag.innerText;
+
+            if(text.length < 7) {
+                pTag.style.fontSize = '2rem';
+            }
+
+            if(text.length > 30) {
+                const fontSize = 46 / text.length;
+                pTag.style.fontSize = `${fontSize}rem`;
+            }
+
+            text.split(' ').forEach(word => {
+                const wordLength = word.length;
+                
+                if(wordLength > 11) {
+                    const fontSize = 11 / wordLength;
+                    pTag.style.fontSize = `${fontSize}rem`;
+                }
+            });
+        }
+    }
 };
 </script>
