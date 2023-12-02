18855dd050c58682d543edaa25eab6820505dff8
Mittelblut9 <info@blackdayz.de>
2023-12-02 23:19:11 +0100

    feat: add buttons

---
+++ src/atoms/edit-button.atom.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="edit-button">
+        <button class="edit-button__button" @click="isEditing = !isEditing"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EditButtonAtom',
+};
+</script>
--- src/views/Homepage.view.vue
+++ src/views/Homepage.view.vue
@@ -1,15 +1,20 @@
 <template>
   <BingoComponent />
+  <SaveButtonAtom />
+  <EditButtonAtom />
 </template>
 
 <script>
+import EditButtonAtom from '@/atoms/edit-button.atom.vue';
+import SaveButtonAtom from '@/atoms/save-button.atom.vue';
 import BingoComponent from '@/components/bingo.component.vue';
-
 
 export default {
     name: 'HomepageView',
     components: {
         BingoComponent,
+        SaveButtonAtom,
+        EditButtonAtom,
     }
 };
 </script>
