ecd1d6722fec701b696a62ea2d6e9327166f7f47
Ben <67608550+Mittelblut9@users.noreply.github.com>
2023-12-05 08:10:17 +0000

    feat: add import button

---
+++ src/atoms/import-button.atom.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="import-button">
+        <button class="btn btn-secondary" @click="importBingo">Import</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ImportButtonAtom',
+    methods: {
+        importBingo() {
+            //todo
+            // - add a file upload html object
+            // - add a fiel upload handler
+            // - read the json file
+            // - check if its valid
+            // - -  if valid, read the data and loop through the table and insert all data
+            // - - (dont save it. the user has to save it manually)
+            // - - if not valid, output the error
+        },
+    }
+};
+</script>
